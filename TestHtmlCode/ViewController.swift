8b7c7295da8181fddd5c5189d303427d88adaf29
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-03-04 16:39:04 +0800

    Initial Commit

---
+++ TestHtmlCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHtmlCode
+//
+//  Created by Bruce Chen on 2019/3/4.
+//  Copyright © 2019 APP技術部-陳冠志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
